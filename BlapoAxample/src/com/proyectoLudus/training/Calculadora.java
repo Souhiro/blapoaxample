51879f0a90cda1d33ccca9547934b8b03f201050
souhiro@gmail.com <souhiro@gmail.com@dd61c729-c436-f87f-488d-e35ca8da5133>
2013-11-24 22:46:54 +0000

    Creada una calculadora

---
+++ BlapoAxample/src/com/proyectoLudus/training/Calculadora.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyectoLudus.training;
+
+/**
+ *
+ * @author Souhiro
+ */
+public class Calculadora {
+    private float valor;
+    
+    public Calculadora() {
+        this.valor = 0;
+    }
+    
+    public void suma(float num) {
+        this.valor = this.valor + num;
+    }
+    
+    public void resta(float num) {
+        this.valor = this.valor - num;
+    }
+    
+    public void multiplicacion(float num) {
+        this.valor = this.valor * num;
+    }
+    
+    public void division(float num) {
+        this.valor = this.valor / num;
+    }
+    
+    public float devuelveValor() {
+        return this.valor
+    }
+}
