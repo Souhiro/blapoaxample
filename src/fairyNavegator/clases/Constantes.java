ff5140567b06a193dbb784804daa2196150cefab
Souhiro <souhiro@gmail.com>
2025-06-28 17:16:20 +0200

    Se evita colision de identidades usando una variable autoincrementada

--- src/fairyNavegator/clases/Constantes.java
+++ src/fairyNavegator/clases/Constantes.java
@@ -1,8 +1,9 @@
 package fairyNavegator.clases;
 
 public class Constantes {
+	private static int auxCounter = 0;
 
-	public static final int LOCALIZACION_INVENTARIO = 255;
+	public static final int LOCALIZACION_INVENTARIO = 60000;
 	
 	public static final int SIN_SALIDA = -1;
 	
@@ -12,6 +13,8 @@ public class Constantes {
 	public static final String SUR = "SUR";
 	public static final String ESTE = "ESTE";
 	public static final String OESTE = "OESTE";
+
+	public static final Object COMANDO_COGER = "COGER";
 	
 	public static final int PERSONA = 1;
 	public static final int ATREZZO = 2;
@@ -19,39 +22,37 @@ public class Constantes {
 	public static final int MUERTO = 100;
 	public static final int ROTO = 101;
 
-	public static final int HAB_ELAIA = 1;
-	public static final int HAB_PASILLO_1 = 2;
-	public static final int HAB_PASILLO_2 = 3;
-	public static final int HAB_NURGLE = 4;
-	public static final int HAB_PASILLO_3 = 5;
-	public static final int HAB_KROSSY = 6;
-	public static final int HAB_SALON = 7;
-	public static final int HAB_COCINA = 8;
-	public static final int HAB_SALIDA = 9;
+	public static final int HAB_ELAIA = ++auxCounter;
+	public static final int HAB_PASILLO_1 = ++auxCounter;
+	public static final int HAB_PASILLO_2 = ++auxCounter;
+	public static final int HAB_NURGLE = ++auxCounter;
+	public static final int HAB_PASILLO_3 = ++auxCounter;
+	public static final int HAB_KROSSY = ++auxCounter;
+	public static final int HAB_SALON = ++auxCounter;
+	public static final int HAB_COCINA = ++auxCounter;
+	public static final int HAB_SALIDA = ++auxCounter;
 
-	public static final int SUB_ESPADA = 1;
-	public static final int SUB_POSTER = 2;
-	public static final int SUB_TACONES = 3;
-	public static final int SUB_BURHKA = 4;
-	public static final int SUB_LIBRO = 5;
-	public static final int SUB_GUITARRA = 6;
-	public static final int SUB_SARTEN = 7;
-	public static final int SUB_ESTROPAJO = 8;
-	public static final int SUB_LLAVE = 9;
+	public static final int SUB_ESPADA = ++auxCounter;
+	public static final int SUB_POSTER = ++auxCounter;
+	public static final int SUB_TACONES = ++auxCounter;
+	public static final int SUB_BURHKA = ++auxCounter;
+	public static final int SUB_LIBRO = ++auxCounter;
+	public static final int SUB_GUITARRA = ++auxCounter;
+	public static final int SUB_SARTEN = ++auxCounter;
+	public static final int SUB_ESTROPAJO = ++auxCounter;
+	public static final int SUB_LLAVE = ++auxCounter;
 
-	public static final int SUB_NURGLE = 100;
-	public static final int SUB_KROSSY = 102;
-	public static final int SUB_THAMOND = 103;
-	public static final int SUB_ENRIC = 105;
-	public static final int SUB_WYNNA = 106;
-	public static final int SUB_CAMA_ELAIA = 107;
-	public static final int SUB_VENTANA_ELAIA = 108;
-	public static final int SUB_SOUNDWAVE = 109;
-	public static final int SUB_PUERTA = 110;
-
-	public static final Object COMANDO_COGER = "COGER";
+	public static final int SUB_NURGLE = ++auxCounter;
+	public static final int SUB_KROSSY = ++auxCounter;
+	public static final int SUB_THAMOND = ++auxCounter;
+	public static final int SUB_ENRIC = ++auxCounter;
+	public static final int SUB_WYNNA = ++auxCounter;
+	public static final int SUB_CAMA_ELAIA = ++auxCounter;
+	public static final int SUB_VENTANA_ELAIA = ++auxCounter;
+	public static final int SUB_SOUNDWAVE = ++auxCounter;
+	public static final int SUB_PUERTA = ++auxCounter;
 	
-	public static final int FLAG_TACONES = 1;
+	public static final int FLAG_TACONES = ++auxCounter;
 
 	 
 }
